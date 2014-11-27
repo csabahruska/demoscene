63396ccc5b02d1ec0d0ec5818f2c9bc136cc8b6c
Péter Diviánszky <divipp@gmail.com>
2014-11-27 23:21:12 +0100

    refactoring

--- KnotsLC.hs
+++ KnotsLC.hs
@@ -14,7 +14,8 @@ import Data.Traversable
 import Control.Applicative hiding (Const)
 import Control.Monad.Writer
 import Control.Monad.State
---import Control.Monad.Cont
+import Control.Monad.Reader
+import Control.Monad.Cont
 import qualified Data.Map as M
 import qualified Data.IntMap as IM
 import qualified Data.ByteString.Char8 as BS
@@ -40,27 +41,21 @@ data Wire
     -- normal
 
 type ST = IM.IntMap (Either (Exp_ Unique) (Exp V Float))
-newtype Cont a = Cont { runCont :: ST -> ((ST, a) -> Exp V Float) -> Exp V Float }
-    deriving (Functor)
-instance Applicative Cont where
-    pure a = Cont $ \st co -> co $ (st, a)
-    Cont f <*> Cont x = Cont $ \st co -> f st $ \(st, f') -> x st $ \(st, x') -> co (st, f' x')
+--type Cont_ = Cont (ST -> Exp V Float)
 
 transExp :: K.Exp -> IO (M.Map String (Exp V Float) -> Exp V Float)
-transExp x = (\(Graph rx x) env -> transExp_ x env (IM.map Left $ IM.fromList rx) snd) <$> reify x
+transExp x = (\(Graph rx x) env -> transExp_ x env (IM.map Left $ IM.fromList rx) const) <$> reify x
    where
     transExp_
         :: Unique
         -> M.Map String (Exp V Float)
         -> ST
-        -> ((ST, Exp V Float) -> Exp V Float)
+        -> (Exp V Float -> ST -> Exp V Float)
         -> Exp V Float
     transExp_ x env st cont_ = case st IM.! x of
-        Right ex -> cont_ (st, ex)
-        Left e -> runCont (traverse (\i -> Cont $ \st co -> transExp_ i env st co) e) st $ \(st, xx) -> let
-            end a = Let a $ \rr -> cont_ (IM.insert x (Right rr) st, rr)
---            end2 f (st, a) (st', b) = Let (f a b) $ \rr -> cont_ (IM.insert x (Right rr) st, rr)
-          in end $ case xx of
+        Right ex -> cont_ ex st
+        Left e -> flip (runCont $ traverse (\i -> cont $ \co st -> transExp_ i env st co) e) st $ \xx st ->
+          flip Let (\rr -> cont_ rr $ IM.insert x (Right rr) st) $ case xx of
             C_ f        -> Const f
             K.Var_ s    -> fromMaybe (Uni (IFloat $ BS.pack s)) $ M.lookup s env
             Add_ e f    -> (@+) e f
@@ -76,52 +71,7 @@ transExp x = (\(Graph rx x) env -> transExp_ x env (IM.map Left $ IM.fromList rx
             K.Exp_ e    -> exp' e
             Log_ e      -> log' e
 
---  (Unique -> Cont X) -> Exp_ Unique -> Cont (Exp_ X)
 
--- Cont X b = (b -> X) -> X
-
-{- 
-         Left mx -> let
-            look e = 
-            ex' = case traverse (transExp_ e env) mx of
-                    C_ f -> Const f
-                    Add_ e f -> e @+ f
-                    Sub_ e f -> e @- f
-                    Mul_ e f -> e @* f
-                    Div_ e f -> e @/ f
-                    Abs_ e -> abs' e
-                    Signum_ e -> sign' e
-                    Sin_ e -> sin' e
-                    Cos_ e -> cos' e
-                    ASin_ e -> asin' e
-                    ACos_ e -> acos' e
-                    K.Exp_ e -> exp' e
-                    Log_ e -> log' e
-                    K.Var_ s -> fromMaybe (Uni (IFloat $ BS.pack s)) $ M.lookup s env
-           in Let ex' $ \exx ->  $ IM.insert x $ Right exx
--}
-{-
-        K.Let e f -> Let tre $ \e' -> flip transExp_ (idc+1, M.insert vname e' $ env) $ f $ K.Var vname
-          where vname = "#" ++ show idc
--}
---      where
-
-        
-
---  [(1, X 2 2), (2, Y)] 1      ->  Let Y $ \e -> Let (X e e) $ \f -> f
---  Exp  (Exp -> Exp)
-
--- use in ghci like ':main -o circle.svg -w 600'
---main = mainWith (dia :: Diagram B R2)
-{-
-dia' = fromVertices points
-  where
-    points = map (toP . project') $ sampleCurve 20 $
--}
-{-
-time :: K.Exp
-time = "time"
--}
 instance Timed K.Exp where
     time = "time"
 
